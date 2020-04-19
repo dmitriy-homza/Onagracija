65d7301d14d8fff3d05797abcaf37b5d4a131611
dmitriy-homza <54332269+dmitriy-homza@users.noreply.github.com>
2020-04-19 17:16:29 +0300

    init: project onagracija

---
+++ js/preloader.js
@@ -0,0 +1,8 @@
+	window.onload = function() {
+        document.getElementById("preloader").classList.add('loaded');
+		setTimeout(function() {
+			document.getElementById("preloader").style.display = "none";
+
+		}, 400);
+
+	};
