1ff3d997c94de743280206077b799f441f9d9d29
dmitriy-homza <54332269+dmitriy-homza@users.noreply.github.com>
2020-05-10 14:53:59 +0300

    add: hoof-trimming page

--- js/preloader+nav.js
+++ js/preloader+nav.js
@@ -7,5 +7,36 @@
 
 	};
 
+	//Бургер
+let burgerIcon = document.querySelector('header img');
+burgerIcon.addEventListener('click', function() {
+  if (burgerIcon.getAttribute('class')=='rotate') {
+    isNotBurger();
+  }
+  else {
+    isBurger();
+  }
+
+})
+
+document.querySelector('.burger-overflow-hidden').addEventListener('click', function() {
+  isNotBurger();
+});
+
+function isBurger () {
+  burgerIcon.classList.add('rotate');
+  setTimeout(() => document.querySelector('.logo').classList.add('opacity'),00);
+  document.querySelector('nav').classList.add('menu-active');
+  document.querySelector('.burger-overflow-hidden').classList.add('burger-overflow-animation');
+  setTimeout(() => document.querySelector('.burger-overflow-hidden').classList.add('burger-overflow'),100);
+}
+
+function isNotBurger () {
+  burgerIcon.classList.remove('rotate');
+  document.querySelector('nav').classList.remove('menu-active');
+  setTimeout(() => document.querySelector('.logo').classList.remove('opacity'),300);
+  document.querySelector('.burger-overflow-hidden').classList.remove('burger-overflow');
+  setTimeout(() => document.querySelector('.burger-overflow-animation').classList.remove('burger-overflow-animation'),500);
+}
 	  
 	  
